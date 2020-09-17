1df035714bc8c6d31007893a580f1e0fbd8614db
Victoria Kahr <victoria.kahr@icloud.com>
2020-09-17 16:41:36 +0200

    challange

---
+++ exercise-04/challange-01.js
@@ -0,0 +1,8 @@
+var Vienna = [
+    ["Vienna", " ", " ", " "],
+    ["\n\t", "is", " ", " ", " "],
+    ["\n\t", " ", "a", " ", " "],
+    ["\n\t", " ", " ", "nice", " "],
+    ["\n\t", " ", " ", " ", "city"]
+];
+console.log(Vienna[0].join(" "), Vienna[1].join(" "), Vienna[2].join(" "), Vienna[3].join(" "), Vienna[4].join(" "));
